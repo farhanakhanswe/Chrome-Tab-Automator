8d0c807711a5d8e208525096d38bb6e4353599f6
Farhana Khan <134283374+farhanakhanswe@users.noreply.github.com>
2025-12-17 00:57:11 +0600

    Open multiple hardcoded URLs automatically

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-chrome.tabs.create({
-    url: "https://www.google.com"
+const bulkUrls = [
+    "https://www.google.com",
+    "https://www.youtube.com/"
+]
+
+bulkUrls.forEach(url => {
+    chrome.tabs.create({
+        url: url
+    });
 });
